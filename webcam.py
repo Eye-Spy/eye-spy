a8328f24de9a460c0e560df0528cb35df7a5beb0
Alce Kerrigan <imor231@gmail.com>
2019-11-10 10:33:55 -0500

    Worked on webcam

--- webcam.py
+++ webcam.py
@@ -1,11 +1,17 @@
 import copy
 import cv2
 import numpy as np
+import imutils
+import traceback
 from tensorflow.keras.models import load_model
 
-LR = 0.1
+
+
+LR = 0.001
 BGS_THRESH = 50
 
+
+AVG_WEIGHT = 0.5
 class WebcamHandler:
 	"""
 	Manages the operation of the webcame
@@ -15,22 +21,10 @@ class WebcamHandler:
 		print("Webcam init")
 
 		self.bgModel = cv2.createBackgroundSubtractorMOG2(0, BGS_THRESH)
-	
-	def segment(self, image, threshold=25):
+		self.hand_model = cv2.CascadeClassifier('aGest.xml')
+		self.ready_frames = 0
 
-		global bg
-
-		diff = cv2.absdiff(bg.astype("uint8"), image)
-
-		(_, contours, _ ) = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-		# No hand detected
-		if len(contours) == 0:
-			return 
-		else:
-
-			segmented = max(contours, key=cv2.contourArea)
-			return (threshold, segmented)
+		self.top, self.right, self.bottom, self.left = 10, 350, 225, 590
 
 	def remove_background(self, frame):
 		"""
@@ -43,19 +37,100 @@ class WebcamHandler:
 		result = cv2.bitwise_and(frame, frame, mask=foreground_mask)
 
 		return result
+	def get_greyscale(self):
+
+		(grabbed, frame) = self.webcam.read()
+		frame = imutils.resize(frame, width=700)
+		frame = cv2.flip(frame, 1)
+
+		clone = frame.copy()
+
+		(height, width) = frame.shape[:2]
+		#region_of_interest = frame[self.top:self.bottom, self.right:self.left]
+		region_of_interest = frame
 
 
-	def run_webcam(self):
+		gray = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (7,7), 0)
 
-		camera = cv2.VideoCapture(0)
-		while(True):
+		return gray, clone
+	
+	def set_background(self):
+		"""
+		Grabs 30 frames from the camera and uses them to 
+		set the background image
+		"""
+
+		# Grab the global background 
+		global bg
+
+		image, _ = self.get_greyscale()
+		
+		# Resets the global background to the image in front
+		# of the screen
+		bg = image.copy().astype("float")
+		for i in range(30):
+			image, _ = self.get_greyscale()
+			# cv2.imshow("test", image)
+			cv2.accumulateWeighted(image, bg, AVG_WEIGHT)
+	
 
 
-			(grabbed,frame) = camera.read()
-			cv2.imshow("Feed", frame)
+	def find_hand(self):
+		try:
+			ret, frame = self.webcam.read()
+			frame = cv2.flip(frame, 1)
+			kernel = np.ones((3,3), np.uint8)
 
-			if cv2.waitKey(1) == 27:
-				break
+			region_of_interest = frame[100:300, 100:300]
+			#region_of_interest = frame
+
+			cv2.rectangle(frame, (100,100), (300,300), (0, 255, 0), 0)
+			hsv = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2HSV)
+
+			skin_l = np.array([0, 20, 70], dtype=np.uint8)
+			skin_h = np.array([20, 255, 255], dtype=np.uint8)
+
+			msk = cv2.inRange(hsv, skin_l, skin_h)
+
+			msk = cv2.dilate(msk, kernel, iterations= 4)
+
+			msk = cv2.GaussianBlur(msk, (5, 5), 100)
+
+			cnt, hier = cv2.findContours(msk, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+			
+			cnt = max(cnt, key = lambda x: cv2.contourArea(x))
+			eps = 0.0005*cv2.arcLength(cnt, True)
+			apx = cv2.approxPolyDP(cnt, eps, True)
+			
+			hull = cv2.convexHull(cnt)
+
+			ahull = cv2.contourArea(hull)
+			areacount = cv2.contourArea(cnt)
+			areart = ((ahull - areacount)/ areacount) * 100
+
+			hull = cv2.convexHull(apx, returnPoints=False)
+			defects = cv2.convexityDefects(apx, hull)
+
+			cv2.imwrite("test.png", frame)
+		except Exception as e:
+			print("Failed: ", e)
+
+			print(''.join(traceback.format_tb(e.__traceback__)))
+			exit(0)
+			return None
+
+
+	def start_webcam(self):
+		self.webcam = cv2.VideoCapture(0)
+
+
+	def main_loop(self):
+
+		while(1):
+			test = self.find_hand()
+		
 
 test = WebcamHandler()
-test.run_webcam()
+test.start_webcam()
+test.main_loop()
