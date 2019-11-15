3c4cd5c6f801e9da27c92df5ee45dccfc5d736e3
Alce Kerrigan <imor231@gmail.com>
2019-11-15 06:36:27 -0500

    updated webcam

--- webcam.py
+++ webcam.py
@@ -1,136 +1,139 @@
-import copy
 import cv2
 import numpy as np
-import imutils
-import traceback
-from tensorflow.keras.models import load_model
+import copy
+import math
+#from appscript import app
+
+# Environment:
+# OS    : Mac OS EL Capitan
+# python: 3.5
+# opencv: 2.4.13
+
+# parameters
+cap_region_x_begin=0.5  # start point/total width
+cap_region_y_end=0.8  # start point/total width
+threshold = 60  #  BINARY threshold
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 50
+learningRate = 0
+
+# variables
+isBgCaptured = 0   # bool, whether the background captured
+triggerSwitch = False  # if true, keyborad simulator works
+
+def printThreshold(thr):
+    print("! Changed threshold to "+str(thr))
 
 
+def removeBG(frame):
+    fgmask = bgModel.apply(frame,learningRate=learningRate)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    # res = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
-LR = 0.001
-BGS_THRESH = 50
-
-
-AVG_WEIGHT = 0.5
-class WebcamHandler:
-	"""
-	Manages the operation of the webcame
-	"""
-
-	def __init__(self):
-		print("Webcam init")
-
-		self.bgModel = cv2.createBackgroundSubtractorMOG2(0, BGS_THRESH)
-		self.hand_model = cv2.CascadeClassifier('aGest.xml')
-		self.ready_frames = 0
-
-		self.top, self.right, self.bottom, self.left = 10, 350, 225, 590
-
-	def remove_background(self, frame):
-		"""
-		Uses OpenCV's built in background subtractor to extreact the 
-		foreground by removing the background
-		"""
-		foreground_mask = self.bgModel.apply(frame, learningRate=LR)
-		kernel = np.ones((3,3), np.uint8)
-		foreground_mask = cv2.erode(foreground_mask, kernel, iterations=1)
-		result = cv2.bitwise_and(frame, frame, mask=foreground_mask)
-
-		return result
-	def get_greyscale(self):
-
-		(grabbed, frame) = self.webcam.read()
-		frame = imutils.resize(frame, width=700)
-		frame = cv2.flip(frame, 1)
-
-		clone = frame.copy()
-
-		(height, width) = frame.shape[:2]
-		#region_of_interest = frame[self.top:self.bottom, self.right:self.left]
-		region_of_interest = frame
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
 
 
-		gray = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7,7), 0)
+def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
+    #  convexity defect
+    hull = cv2.convexHull(res, returnPoints=False)
+    if len(hull) > 3:
+        defects = cv2.convexityDefects(res, hull)
+        if type(defects) != type(None):  # avoid crashing.   (BUG not found)
 
-		return gray, clone
-	
-	def set_background(self):
-		"""
-		Grabs 30 frames from the camera and uses them to 
-		set the background image
-		"""
-
-		# Grab the global background 
-		global bg
-
-		image, _ = self.get_greyscale()
-		
-		# Resets the global background to the image in front
-		# of the screen
-		bg = image.copy().astype("float")
-		for i in range(30):
-			image, _ = self.get_greyscale()
-			# cv2.imshow("test", image)
-			cv2.accumulateWeighted(image, bg, AVG_WEIGHT)
-	
+            cnt = 0
+            for i in range(defects.shape[0]):  # calculate the angle
+                s, e, f, d = defects[i][0]
+                start = tuple(res[s][0])
+                end = tuple(res[e][0])
+                far = tuple(res[f][0])
+                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
+                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
+                    cnt += 1
+                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
+            return True, cnt
+    return False, 0
 
 
-	def find_hand(self):
-		try:
-			ret, frame = self.webcam.read()
-			frame = cv2.flip(frame, 1)
-			kernel = np.ones((3,3), np.uint8)
-
-			region_of_interest = frame[100:300, 100:300]
-			#region_of_interest = frame
-
-			cv2.rectangle(frame, (100,100), (300,300), (0, 255, 0), 0)
-			hsv = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2HSV)
-
-			skin_l = np.array([0, 20, 70], dtype=np.uint8)
-			skin_h = np.array([20, 255, 255], dtype=np.uint8)
-
-			msk = cv2.inRange(hsv, skin_l, skin_h)
-
-			msk = cv2.dilate(msk, kernel, iterations= 4)
-
-			msk = cv2.GaussianBlur(msk, (5, 5), 100)
-
-			cnt, hier = cv2.findContours(msk, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-			
-			cnt = max(cnt, key = lambda x: cv2.contourArea(x))
-			eps = 0.0005*cv2.arcLength(cnt, True)
-			apx = cv2.approxPolyDP(cnt, eps, True)
-			
-			hull = cv2.convexHull(cnt)
-
-			ahull = cv2.contourArea(hull)
-			areacount = cv2.contourArea(cnt)
-			areart = ((ahull - areacount)/ areacount) * 100
-
-			hull = cv2.convexHull(apx, returnPoints=False)
-			defects = cv2.convexityDefects(apx, hull)
-
-			cv2.imwrite("test.png", frame)
-		except Exception as e:
-			print("Failed: ", e)
-
-			print(''.join(traceback.format_tb(e.__traceback__)))
-			exit(0)
-			return None
+# Camera
+camera = cv2.VideoCapture(0)
+camera.set(10,200)
+cv2.namedWindow('trackbar')
+cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
 
 
-	def start_webcam(self):
-		self.webcam = cv2.VideoCapture(0)
+while camera.isOpened():
+    ret, frame = camera.read()
+    threshold = cv2.getTrackbarPos('trh1', 'trackbar')
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                 (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+    cv2.imshow('original', frame)
 
+    #  Main operation
+    if isBgCaptured == 1:  # this part wont run until background captured
+        img = removeBG(frame)
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+                    int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        #cv2.imshow('mask', img)
 
-	def main_loop(self):
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        #cv2.imshow('blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
+        cv2.imshow('ori', thresh)
 
-		while(1):
-			test = self.find_hand()
-		
+        
+        # get the coutours
+        thresh1 = copy.deepcopy(thresh)
+        contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        length = len(contours)
+        maxArea = -1
+        if length > 0:
+            for i in range(length):  # find the biggest contour (according to area)
+                temp = contours[i]
+                area = cv2.contourArea(temp)
+                if area > maxArea:
+                    maxArea = area
+                    ci = i
 
-test = WebcamHandler()
-test.start_webcam()
-test.main_loop()
+            res = contours[ci]
+            hull = cv2.convexHull(res)
+            drawing = np.zeros(img.shape, np.uint8)
+            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
+            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
+
+            isFinishCal,cnt = calculateFingers(res,drawing)
+            if triggerSwitch is True:
+                if isFinishCal is True and cnt <= 2:
+                    print (cnt)
+                    #app('System Events').keystroke(' ')  # simulate pressing blank space
+                    
+
+        cv2.imshow('output', drawing)
+
+    # Keyboard OP
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit
+        camera.release()
+        cv2.destroyAllWindows()
+        break
+    elif k == ord('b'):  # press 'b' to capture the background
+        bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+        isBgCaptured = 1
+        print( '!!!Background Captured!!!')
+    elif k == ord('r'):  # press 'r' to reset the background
+        bgModel = None
+        triggerSwitch = False
+        isBgCaptured = 0
+        print ('!!!Reset BackGround!!!')
+    elif k == ord('n'):
+        triggerSwitch = True
+        print ('!!!Trigger On!!!')
