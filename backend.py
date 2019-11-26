08b59fcb2804b0a6dab0688d0ed9399aeff23d9d
Spencer Ross <ser8@cfl.rr.com>
2019-11-26 15:56:40 -0500

    Some quck updates

--- backend.py
+++ backend.py
@@ -1,4 +1,50 @@
-edef gesture_switch(sample):
+import WebcamHandler
+
+
+def profile1():
+    return "profile1"
+
+def profile2():
+    return "profile2"
+
+def profile3():
+    return "profile3"
+
+def profile4():
+    return "profile4"
+
+def profile5():
+    return "profile5"
+
+def profile6():
+    return "profile6"
+
+def profile7():
+    return "profile7"
+
+def profile8():
+    return "profile8"
+
+def profile9():
+    return "profile9"
+
+def profile10():
+    return "profile10"
+
+
+
+
+#Get the Gesture
+
+WebcamHandler.webcam.get_gesture()
+
+
+
+
+
+
+
+def gesture_switch(sample):
         switcher = {
             1: "test",
             2: "basic",
