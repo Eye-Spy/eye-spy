af6ba3495d44504f0e0acb5cc2e81a715679fca0
Jake Meyer <jakemeyer@html-trojan-rr.home>
2019-11-26 15:55:46 -0500

    Changes made to config schema to make it readable, functions added in UserProfile.py

--- UserProfile.py
+++ UserProfile.py
@@ -1,3 +1,4 @@
+import json
 class UserProfile():
 
     def __init__(self):
@@ -5,3 +6,34 @@ class UserProfile():
 
     def to_dict(self):
         return
+            
+
+    def add_mapping(profile_id, gesture_id, application):
+        try:
+            with open('./config.json', 'r+') as mappings:
+                data = json.load(mappings)
+                data['user_profiles'][profile_id - 1]['gestures'][gesture_id - 1]['mappings'].append(application)
+                mappings.seek(0)
+                json.dump(data, mappings, indent=4)
+                mappings.truncate()
+        except (IndexError):
+                print("Invalid Index Value")
+        
+    def remove_mapping(profile_id, gesture_id, application):
+        try:
+            with open('./config.json', 'r+') as mappings:
+                data = json.load(mappings)
+                data['user_profiles'][profile_id - 1]['gestures'][gesture_id - 1]['mappings'].remove(application)
+                mappings.seek(0)
+                json.dump(data, mappings, indent=4)
+                mappings.truncate()
+        except (ValueError):
+                print("Invalid Application Value")
+
+    def get_mapping(profile_id, gesture_id):
+        try:
+            with open('./config.json', 'r') as mappings:
+                data = json.load(mappings)
+                return data['user_profiles'][profile_id - 1]['gestures'][gesture_id - 1]['mappings']
+        except (IndexError):
+                print("Invalid Index Value")
