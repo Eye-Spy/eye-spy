c36582cc502a1c8b82dd4b1776c6a8f349857ae9
Jake Meyer <jakemeyer@html-trojan-rr.lan>
2019-11-29 08:13:04 -0800

    GUI revamp + supporting functions

---
+++ GestureNames.py
@@ -0,0 +1,12 @@
+gesture_dict = {
+    1 : "One Finger",
+    2 : "Two Fingers",
+    3 : "Three Fingers",
+    4 : "Four Fingers",
+    5 : "Five Fingers",
+    6 : "Fist",
+    7 : "Thumbs Up",
+    8 : "Thumbs Down",
+    9 : "Okay",
+    10: "C Hand"
+}
--- UserProfile.py
+++ UserProfile.py
@@ -1,12 +1,51 @@
 import json
+from tkinter import *
+import GestureNames
+
 class UserProfile():
 
     def __init__(self):
         return
 
-    def to_dict(self):
-        return
-            
+    def populate_profile_listbox(listbox):
+        try:
+            with open('./config.json', 'r') as mappings:
+                data = json.load(mappings)
+                for each in data['user_profiles']:
+                    listbox.insert(END, each['profile_id'])
+        except (IndexError):
+                #handle what happens when we have greater than 10 user profiles
+                return
+
+    def populate_gestures_listbox(listbox):
+        try:
+            for each in GestureNames.gesture_dict.values():
+                listbox.insert(END, each)
+        except (IndexError):
+                #handle what happens when we have greater than 10 user profiles
+                return
+
+    def populate_mappings_listbox(listbox, profile_id, gesture_id):
+        try:
+            with open('./config.json', 'r') as mappings:
+                data = json.load(mappings)
+                for each in data['user_profiles'][profile_id]['gestures'][gesture_id]['mappings']:
+                    listbox.insert(END, each)
+        except (IndexError):
+                #handle what happens when we have greater than 10 user profiles
+                return
+
+    def change_profile_id(index_profile_id, new_Username):
+        try:
+            with open('./config.json', 'r+') as mappings:
+                data = json.load(mappings)
+                data['user_profiles'][index_profile_id]['profile_id'] = new_Username
+                mappings.seek(0)
+                json.dump(data, mappings, indent=4)
+                mappings.truncate()
+        except (IndexError):
+                #handle what happens when we have greater than 10 user profiles
+                return
 
     def add_mapping(profile_id, gesture_id, application):
         try:
