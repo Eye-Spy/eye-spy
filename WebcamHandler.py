c0342ffabe01d8b4cf73bac97e6b29fb48018861
Alce Kerrigan <imor231@gmail.com>
2019-11-30 09:28:46 -0500

    Changed test process

--- WebcamHandler.py
+++ WebcamHandler.py
@@ -14,7 +14,27 @@ from tensorflow.keras.losses import categorical_crossentropy
 
 import cv2
 
-import Constants
+# Parameters for vision
+threshold = 60
+blurValue = 41
+bgSubThreshold = 50
+learningRate = 0
+
+CONFIDENCE_REQ = 20
+
+class_dict = {
+    0 : "noise",
+    1 : "one_finger",
+    2 : "two_fingers",
+    3 : "three_fingers",
+    4 : "four_fingers",
+    5 : "five_fingers",
+    6 : "fist",
+    7 : "thumbs_up",
+    8 : "thumbs_down",
+    9 : "okay",
+    10: "c_hand"
+}
 
 
 class WebcamHandler(Thread):
@@ -146,3 +166,9 @@ class WebcamHandler(Thread):
 if __name__ == "__main__":
     test = WebcamHandler()
     test.start()
+    while(1):
+        try:
+            image = test.cur_image
+            cv2.imshow("test", image)
+        except:
+            pass
